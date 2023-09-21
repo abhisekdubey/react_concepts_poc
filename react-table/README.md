f092463c13a478ed8645c960e72eeb2f307be647
Abhisek Dubey <abhidb95@gmail.com>
2023-09-21 22:47:36 +0530

    Project 01 : React_table basic setup done

---
+++ react-table/README.md
@@ -0,0 +1,7 @@
+**** React Table Project
+
+react-table npm
+https://www.npmjs.com/package/react-table
+
+tanstack react-table
+https://tanstack.com/table/v8/docs/guide/introduction
---
+++ react-table/src/App.js
@@ -0,0 +1,104 @@
+import './App.css';
+import { useTable } from 'react-table';
+
+const data = [
+  {
+    id: 1,
+    gender: 'Male',
+    salary: 55000
+  },
+  {
+    id: 2,
+    gender: 'Male',
+    salary: 60000
+  },
+  {
+    id: 3,
+    gender: 'Female',
+    salary: 45000
+  },
+
+]
+
+const columns = [
+  {
+    Header: 'ID', // header which is show in ui
+    accessor: 'id' // accessor is like id for header
+  },
+  {
+    Header: 'Gender',
+    accessor: 'gender'
+  },
+  {
+    Header: 'Salary',
+    accessor: 'salary'
+  },
+]
+
+function App() {
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    // columns: [{
+    //   Header: '',
+    //   accessor: 'id'
+    // }],
+    columns,
+    data
+  })
+
+  return (
+    <div className="container">
+      <table {...getTableProps()}>
+        <thead>
+
+          {
+            headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+
+                {
+                  headerGroup.headers.map(header => (
+                    <th {...header.getHeaderProps()}>
+                      {header.render("Header")}
+                    </th>
+                  ))
+                }
+
+              </tr>
+            ))
+          }
+
+          {/* <tr>
+            <th>Id</th>
+            <th>Gender</th>
+            <th>Salary</th>
+          </tr> */}
+        </thead>
+
+        <tbody {...getTableBodyProps()}>
+          {
+            rows.map(row => {
+              prepareRow(row)
+
+              return <tr {...row.getRowProps()}>
+                {
+                  row.cells.map(cell => (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  ))
+                }
+              </tr>
+            })
+          }
+
+
+          {/* <tr>
+            <td>1</td>
+            <td>Male</td>
+            <td>55000</td>
+          </tr> */}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
